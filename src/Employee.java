199ac402efb33fd4861e1c607ec38aa86c8a88ae
Jan Gevaert <jan.gevaert@gmail.com>
2020-12-30 13:15:46 -0700

    Played around with some methods.

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Employee {
   private String firstName;
   private String lastName;
@@ -31,6 +33,24 @@ public class Employee {
 
   public void setId(int id) {
     this.id = id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Employee employee = (Employee) o;
+    return id == employee.id && firstName.equals(employee.firstName) && lastName
+        .equals(employee.lastName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, lastName, id);
   }
 
   @Override
--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,32 @@ public class Main {
     employeeList.add(new Employee("Mary", "Smith", 22));
     employeeList.add(new Employee("Mike", "Wilson", 3245));
 
+    employeeList.forEach(employee -> System.out.println(employee));
+
+    System.out.println("\n" + employeeList.get(1));
+
+    System.out.println("\n" + employeeList.isEmpty() + "\n");
+
+    employeeList.set(1, new Employee("John", "Adams", 4568));
+    employeeList.forEach(employee -> System.out.println(employee));
+
+    System.out.println("\n" + employeeList.size() + "\n");
+
+    employeeList.add(3, new Employee("John", "Doe", 4567));
+    employeeList.forEach(employee -> System.out.println(employee));
+
+    Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
+    for (Employee employee : employeeArray) {
+      System.out.println(employee);
+    }
+
+    System.out.println("\n" + employeeList.contains(new Employee("Mary", "Smith", 22)));
+
+    System.out.println("\n" + employeeList.indexOf(new Employee("John", "Doe", 4567)));
+
+    employeeList.remove(2);
+    employeeList.forEach(employee -> System.out.println(employee));
+
   }
 
 }
